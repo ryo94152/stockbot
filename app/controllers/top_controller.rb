afb3f47a1ef3e173e4366780bcef5e43d99137e5
ryo94152 <ryo94152@gmail.com>
2020-01-28 13:54:20 +0900

    devise関連の記述
    # What
    ログイン関連の記述を追記
    # Why
    一部機能をログイン時のみにするため

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -3,4 +3,5 @@ class TopController < ApplicationController
   
   def index
   end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_27_052445) do
+ActiveRecord::Schema.define(version: 2020_01_27_141815) do
 
   create_table "categories", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "code"
@@ -51,6 +51,18 @@ ActiveRecord::Schema.define(version: 2020_01_27_052445) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "fk_rails_6cf2218501"
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "deliveries", "customers"
