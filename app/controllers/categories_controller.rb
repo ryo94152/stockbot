e1794c3a886af45204de42f05457a7ddcdc2ebf5
ryo94152 <ryo94152@gmail.com>
2020-01-30 01:39:30 +0900

    Update categories_controller.rb

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,6 @@ class CategoriesController < ApplicationController
   skip_before_action:logged_in_user
   
   def index
-    @parents = Category.all.order("id ASC").offset(90)
+    @parents = Category.all.order("id ASC")
   end
 end
