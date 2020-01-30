43f3daf9ecb7823ba7194714dff7b2b2986cf4a4
ryo94152 <ryo94152@gmail.com>
2020-01-30 15:10:42 +0900

    入庫情報編集機能実装

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,25 @@ class CategoriesController < ApplicationController
   skip_before_action:logged_in_user
   
   def index
-    # @parents = Category.all.order("id ASC").offset(300)
+    @parents = Category.all.order("id ASC")
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(save_params)
+    if @category.save
+      redirect_to categories_path
+    else
+      redirect_to new_customer_path
+    end
+  end
+
+  private
+  def save_params
+    params.require(:category).permit(:code,:genre)
+  end
+
 end
